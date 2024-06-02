eb6f2984ffaf724480eef292ecf8d8df0fbe59ab
RiteshSaha <riteshsaha662@gmail.com>
2024-06-02 12:30:53 +0530

    first commit

---
+++ node-js-server/app/config/db.config.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//   HOST: "0.0.0.0",
+//   PORT: 27017,
+//   DB: "Student_db"
+// };
+module.exports = {
+  // Connection string format for MongoDB Atlas
+  URI: "mongodb+srv://sonuh269:9YTY625IF2HFloS0@cluster0.gwppitp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+};
